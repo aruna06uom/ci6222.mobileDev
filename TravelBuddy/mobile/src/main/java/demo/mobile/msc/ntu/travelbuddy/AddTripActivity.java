b036fbda6534b6900e056a99d50fb20a9b3d3be2
Aruna Jayasena <arunajayasena@MacBook-Air.local>
2018-10-06 20:44:37 +0800

    adding initial projet

---
+++ TravelBuddy/mobile/src/main/java/demo/mobile/msc/ntu/travelbuddy/AddTripActivity.java
@@ -0,0 +1,75 @@
+package demo.mobile.msc.ntu.travelbuddy;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class AddTripActivity extends AppCompatActivity {
+
+
+    DatePickerDialog datePickerDialog;
+    EditText startDate;
+    EditText endDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+
+
+
+    public void onStartDateClick(View v) {
+
+        startDate = (EditText) findViewById(R.id.startdate);
+        // calender class's instance and get current date , month and year from calender
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+        // date picker dialog
+        datePickerDialog = new DatePickerDialog(AddTripActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // set day of month , month and year value in the edit text
+                        startDate.setText(dayOfMonth + "/"
+                                + (monthOfYear + 1) + "/" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
+    public void onEndDateClick(View v) {
+
+        endDate = (EditText) findViewById(R.id.enddate);
+        // calender class's instance and get current date , month and year from calender
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+        // date picker dialog
+        datePickerDialog = new DatePickerDialog(AddTripActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // set day of month , month and year value in the edit text
+                        endDate.setText(dayOfMonth + "/"
+                                + (monthOfYear + 1) + "/" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+}
---
+++ TravelBuddy/mobile/src/main/java/demo/mobile/msc/ntu/travelbuddy/MainActivity.java
@@ -0,0 +1,23 @@
+package demo.mobile.msc.ntu.travelbuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void addNewTrip(View view) {
+
+        // Do something in response to button
+        Intent intent = new Intent(this, AddTripActivity.class);
+        startActivity(intent);
+    }
+}
